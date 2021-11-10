8a19562097770ad7d5d9ee0d007fbd31c5b87d2c
Umer ALi Sajid <umer.ali@frag-games.com>
2021-11-10 15:49:25 +0500

    deployment script crush address changed

--- migrations/2_deployments.js
+++ migrations/2_deployments.js
@@ -6,51 +6,38 @@ const CRUSHToken = artifacts.require("CRUSHToken");
 
 module.exports = async function (deployer) {
     //for bitcrush
-    //deployer.deploy(BitcrushStaking, "0xa3ca5df2938126bae7c0df74d3132b5f72bda0b6",500000000000000000n,"0x49B7c429C7B45656580143653C5438862266469f");
-    await deployer.deploy(CRUSHToken);
-    let crushInstance = await CRUSHToken.deployed();
- 
-    await deployer.deploy(BitcrushStaking, crushInstance.address,1000000000000000000n,"0x0E5De84bFC1A9799a0FdA4eF0Bd13b6A20e97d89");
+    
+    await deployer.deploy(BitcrushStaking, "0x0ef0626736c2d484a792508e99949736d0af807e",1000000000000000000n,"0x0E5De84bFC1A9799a0FdA4eF0Bd13b6A20e97d89");
     let stakingInstance = await BitcrushStaking.deployed();
     
-    await deployer.deploy(BitcrushBankroll, crushInstance.address, stakingInstance.address, "0x0E5De84bFC1A9799a0FdA4eF0Bd13b6A20e97d89", "0x0E5De84bFC1A9799a0FdA4eF0Bd13b6A20e97d89", 6000,1000,200,2700 );
+    await deployer.deploy(BitcrushBankroll, "0x0ef0626736c2d484a792508e99949736d0af807e", stakingInstance.address, "0x0E5De84bFC1A9799a0FdA4eF0Bd13b6A20e97d89", "0x0E5De84bFC1A9799a0FdA4eF0Bd13b6A20e97d89", 6000,1000,200,2700, "0x0E5De84bFC1A9799a0FdA4eF0Bd13b6A20e97d89" );
     let bankrollInstance = await BitcrushBankroll.deployed();
     
-    await deployer.deploy(BitcrushLiveWallet, crushInstance.address, bankrollInstance.address,"0x0E5De84bFC1A9799a0FdA4eF0Bd13b6A20e97d89");
+    await deployer.deploy(BitcrushLiveWallet, "0x0ef0626736c2d484a792508e99949736d0af807e", bankrollInstance.address,"0x0E5De84bFC1A9799a0FdA4eF0Bd13b6A20e97d89");
     let liveWalletInstance = await BitcrushLiveWallet.deployed();
-    // await deployer.deploy(BitcrushStaking, "0xa3ca5df2938126bae7c0df74d3132b5f72bda0b6",1000000000000000000n,"0x0E5De84bFC1A9799a0FdA4eF0Bd13b6A20e97d89");
-    // let stakingInstance = await BitcrushStaking.deployed();
-    
-    // await deployer.deploy(BitcrushBankroll, "0xa3ca5df2938126bae7c0df74d3132b5f72bda0b6", stakingInstance.address, "0x0E5De84bFC1A9799a0FdA4eF0Bd13b6A20e97d89", "0x0E5De84bFC1A9799a0FdA4eF0Bd13b6A20e97d89", 6000,1000,200,2700 );
-    // let bankrollInstance = await BitcrushBankroll.deployed();
-    
-    // await deployer.deploy(BitcrushLiveWallet, "0xa3ca5df2938126bae7c0df74d3132b5f72bda0b6", bankrollInstance.address,"0x0E5De84bFC1A9799a0FdA4eF0Bd13b6A20e97d89");
-    // let liveWalletInstance = await BitcrushLiveWallet.deployed();
     
     await stakingInstance.setBankroll(bankrollInstance.address);
     await stakingInstance.setLiveWallet(liveWalletInstance.address);
 
-    await bankrollInstance.setLiveWallet(liveWalletInstance.address);
-    await bankrollInstance.setBitcrushStaking (stakingInstance.address);
+    //await bankrollInstance.setLiveWallet(liveWalletInstance.address);
+    //await bankrollInstance.setBitcrushStaking (stakingInstance.address);
 
-    await liveWalletInstance.setBitcrushBankroll(bankrollInstance.address);
+    //await liveWalletInstance.setBitcrushBankroll(bankrollInstance.address);
     await liveWalletInstance.setStakingPool(stakingInstance.address);
 
     
     
     
-    // let crush = await CRUSHToken.at("0xa3ca5df2938126bae7c0df74d3132b5f72bda0b6");
-    // await crush.approve(bankrollInstance.address,150000000000000000000n);
-    // await bankrollInstance.addToBankroll(150000000000000000000n);
-    // await crush.approve(stakingInstance.address,10000000000000000000000n);
-    // await stakingInstance.addRewardToPool(10000000000000000000000n); 
-    // await stakingInstance.setAutoCompoundLimit(1);
-    // await bankrollInstance.setProfitThreshold(100000000000000000000n); 
-    // await liveWalletInstance.setLockPeriod(259200);
+    let crush = await CRUSHToken.at("0x0ef0626736c2d484a792508e99949736d0af807e");
+    await crush.approve(bankrollInstance.address,150000000000000000000n);
+    await bankrollInstance.addToBankroll(150000000000000000000n);
+    await crush.approve(stakingInstance.address,10000000000000000000000n);
+    await stakingInstance.addRewardToPool(10000000000000000000000n); 
+    await stakingInstance.setAutoCompoundLimit(1);
+    await bankrollInstance.setProfitThreshold(100000000000000000000n); 
+    await liveWalletInstance.setLockPeriod(259200);
     
-    // await bankrollInstance.authorizeAddress(liveWalletInstance.address); 
-
-
+    await bankrollInstance.authorizeAddress(liveWalletInstance.address); 
     //---------------------
     //for live wallet update
    /*  let stakingInstance = await BitcrushStaking.at("0x83f47386e243461AAcE9Fd60cCbdF64D8c96731E");
