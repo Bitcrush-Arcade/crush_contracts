d3c6d4bf6ae18cc789142c9c438a14de7f53ddec
jmanywhere <josmfernandez@gmail.com>
2021-12-07 10:06:44 -0600

    for test env

--- migrations/2_deployments.js
+++ migrations/2_deployments.js
@@ -4,9 +4,9 @@ const CRUSHToken = artifacts.require('CRUSHToken');
 const Lottery = artifacts.require('BitcrushLottery');
 
 module.exports = async function ( deployer ) {
-   // await deployer.deploy(CRUSHToken)
-   // const token = await CRUSHToken.deployed()
-   // deployer.deploy(Lottery, token.address );
-   deployer.deploy(Lottery, "0xa3ca5df2938126baE7c0Df74D3132b5f72bdA0b6" );
+   await deployer.deploy(CRUSHToken)
+   const token = await CRUSHToken.deployed()
+   deployer.deploy(Lottery, token.address );
+   // deployer.deploy(Lottery, "0xa3ca5df2938126baE7c0Df74D3132b5f72bdA0b6" );
    
 }
