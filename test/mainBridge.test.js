cec16b2521dda8d177faf750b0bc7d1af37651be
Yawgmoth19 <urzasguilt4@gmail.com>
2022-01-25 20:14:05 -0600

    Comments and addToken test

--- test/mainBridge.test.js
+++ test/mainBridge.test.js
@@ -4,10 +4,13 @@ const { BN, web3 } = require('@openzeppelin/test-helpers/src/setup');
 const MetaCoin = artifacts.require("MetaCoin");
 const MetaCoin = artifacts.require("MainBridge");
 
-//accounts[0] contract owner address
-//accounts[1] user address
-//accounts[2] bridge address
-//accounts[3] gateway address
+// These tests are for a general bridge contract that can work both as a main chain bridge (lock/unlock) and as a side bridge (mint/burn). 
+// In order to bridge or recieve tokens correctly, the tipe of bridge involved in the other chain has to be specified when adding
+// the token with the addToken function. 
+// accounts[0] contract owner address
+// accounts[1] user address
+// accounts[2] bridge address
+// accounts[3] gateway address
 
 contract('mainBridgeTest', (accounts) => {
   beforeEach( async() => {
@@ -20,21 +23,21 @@ contract('mainBridgeTest', (accounts) => {
   //setFee(uint 256 amount) onlyOwner
   it('Should allow only owner to edit the bridge fee', async() => {
 
-    //Checking if starting fee value is 0
+    // Checking if starting fee value is 0
     const initialFee = new BN(await this.bridge.fee()).toString();
     assert.equal(fee, '0', 'fee should be 0'); 
 
-    //Checking if onlyOwner
+    // Checking if onlyOwner
     await expectRevert(this.bridge.setFee(1, {from: accounts[1]}), 'onlyOwner');
   
-    //Setting fee
+    // Setting fee
     await this.bridge.setFee(1, {from: accounts[0]});
     const finalFee = new BN(await this.bridge.fee()).toString();
     assert.equal(fee, '1', 'fee is not assigned correctly'); 
 
   });
 
-  //addValidChain(uint256 chainId) onlyOwner
+  //addValidChain(struct chainId) onlyOwner
   it('Should allow owner only to add valid chainId', async() => {
   
     //Checking if onlyOwner
@@ -44,104 +47,106 @@ contract('mainBridgeTest', (accounts) => {
     const isValid = await this.bridge.validChains('2222');
     assert.ok(!isValid, 'Chain was already valid');
 
-    //Adding valid chainId to list and checking if it's added
+    //Adding valid chainId 
     await this.bridge.addValidChain(2222, {from: accounts[0]});
     const validChain = await this.bridge.validChains('2222');
     assert.ok(validChain, 'Chain was not valid');
 
   });
 
-  //addToken(uint256 tokenAddress, uint8 contractType) onlyOwner, 0 if invalid, 1 if mint/burn, 2 if lock/unlock
-  //ADD ALL CASES TO TEST
+  // addToken(address tokenAddress, uint 256 tokenFee, bool bridgeType, bool status) onlyOwner.
+  // bridgeType is the type of bridge implemented on target chain. True for lock/unlock, false for mint/burn.
+  // tokenMap stores all the implemented token contracts in other chains.
+  // Fee has a min and a max. 
   it('Should allow owner only to add valid chainId', async() => {
-  
-    //Checking if onlyOwner
-    await expectRevert(this.bridge.addToken(2222, {from: accounts[1]}), 'onlyOwner');
-        
-    //Checking if chain was already valid
-    const isValid = await this.bridge.validChains('2222');
-    assert.ok(!isValid, 'Chain was already valid');
 
-    //Adding valid chainId to list and checking if it's added
-    await this.bridge.addValidChain(2222, {from: accounts[0]});
-    const validChain = await this.bridge.validChains('2222');
-    assert.ok(validChain, 'Chain was not valid');
+    // Checking if onlyOwner
+    await expectRevert(this.bridge.addToken(1111, 1, false, true, {from: accounts[1]}), 'onlyOwner');
+        
+    // Checking if token was already added
+    const isValid = await this.bridge.tokenMap('1111').status;
+    assert.ok(!isValid, 'Token was already added');
+
+    // Adding token
+    await this.bridge.addToken(this.bridge.addToken(1111, 1, false, true, {from: accounts[0]}));
+    const addedToken = await this.bridge.tokenMap('1111').status;
+    assert.ok(addedToken, 'Token was not added');
 
   });
 
 
-  //RequestBridge(address userAddress, uint256 chainId, uint256 tokenContractAddress, uint256 amount) external
+  // RequestBridge(address userAddress, uint256 chainId, uint256 tokenContractAddress, uint256 amount) external
   it('Should allow user to request bridge to send tokens to another chain', async () => {
 
-    //Setting fee amount
+    // Setting fee amount
     await this.bridge.setFee(1, {from: accounts[0]});
     const fee = new BN(await this.bridge.fee).toString();
     assert.equal(fee, '1', 'fee is not assigned correctly'); 
 
-    //Adding valid chain
+    // Adding valid chain
     await this.bridge.addValidChain(2222, {from: accounts[0]});
 
-    //Minting tokens to user
+    // Minting tokens to user
     await this.token.mint(accounts[1], 5,{ from: accounts[0]});
 
-    //Checking that the bridge only happens to valid chain ID. Valid chain ID is string '2222', checking revert first.
+    // Checking that the bridge only happens to valid chain ID. Valid chain ID is string '2222', checking revert first.
     await expectRevert(this.token.requestBridge(1111, 1234, 3, {from: accounts[3]}), 'requestBridge to invalid chainId' );
 
-    //Checking if onlyGateway
+    // Checking if onlyGateway
     await expectRevert(this.token.requestBridge(1111, 2222, 3, {from: accounts[1]}), 'requestBridge can only be called by gateway');
 
-    //Executing the function from Gateway address and valid chainId
+    // Executing the function from Gateway address and valid chainId
     await this.bridge.requestBridge(1111, 2222, 3, {from: accounts[3]});
     const finalBalance = new BN(await this.bridge.balanceOf(accounts[1])).toString();
     assert.equal(finalBalance, '2', 'Tokens are not burned from user account correctly');
 
-    //Checking that nonce/hash is created correctly. REVISAR ARGUMENTOS DE genTxnHash
+    // Checking that nonce/hash is created correctly. REVISAR ARGUMENTOS DE genTxnHash
     const txnHash = await this.bridge.genTxnHash()   
     assert.ok(txnHash, 'txn hash not created');
        
   });
 
-  //sendTransactionSuccess(Hash) onlyBridge
+  // sendTransactionSuccess(Hash) onlyBridge
   it('Should emit bridge success', async() => {
 
-    //Checking that nonce/hash is created. REVISAR ARGUMENTOS DE GENTXNHASH
+    // Checking that nonce/hash is created. REVISAR ARGUMENTOS DE GENTXNHASH
     const txnHash = await this.bridge.genTxnHash({from: accounts[2]});   
 
-    //Checking if onlyGateway
+    // Checking if onlyGateway
     await expectRevert(this.bridge.sendTransactionSuccess(txnHash, {from: accounts[1]}), 'Transaction success event should only be emitted by gateway');
 
-    //Checking if transaction success event is being emitted
+    // Checking if transaction success event is being emitted
     await this.bridge.sendTransactionSuccess(txnHash, {from: accounts[3]});
     assert.ok(txnHash, 'txn success event is not being emitted');
 
   });
 
-  //sendTransactionFailiure(uint 256 txnHash, address userAddress, amount) onlyBridge
+  // sendTransactionFailiure(uint 256 txnHash, address userAddress, amount) onlyBridge
   it('Should emit bridge failiure and refund', async() => {
-    //Checking that nonce/hash is created. REVISAR ARGUMENTOS DE GENTXNHASH
+    // Checking that nonce/hash is created. REVISAR ARGUMENTOS DE GENTXNHASH
     const txnHash = await this.bridge.genTxnHash({from: accounts[2]});   
   
-    //Checking if onlyGateway
+    // Checking if onlyGateway
     await expectRevert(this.bridge.sendTransactionFailiure(txnHash, {from: accounts[1]}), 'Transaction success event should only be emitted by gateway');
 
-    //Checking transaction failiure
+    // Checking transaction failiure
     await this.bridge.sendTransactionFailiure(txnHash, accounts[1], 2, {from: accounts[3]});
     const userFinalBalance = new BN(await this.token.balanceOf(accounts[1])).toString();
     
-    //Checking if function was executed and if it refunded 
+    // Checking if function was executed and if it refunded 
     assert.ok(txnHash, 'txn success event is not being emitted');
     assert.equal(userFinalBalance, '3', 'Amount is not being refunded');
     
   });
 
-  //recieveTransactionSuccess(userAddress, amount, fromChainID) onlyGateway
+  // recieveTransactionSuccess(userAddress, amount, fromChainID) onlyGateway
   it('Should recieve transaction success', async() => {
 
-    //Recieving from valid chain, should mint to user
+    // Recieving from valid chain, should mint to user
     await this.bridge.recieveTransactionSuccess(accounts[1], 3, '2222', {from: accounts[3]});
     const userFinalBalance = new BN(await this.token.balanceOf(accounts[1])).toString();
       
-    //Checking if function was executed and if it minted
+    // Checking if function was executed and if it minted
     assert.ok(txnHash, 'Recieved txn success event is not being emitted');
     assert.equal(userFinalBalance, '3', 'Amount is not being refunded');
 
