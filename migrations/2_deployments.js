5c39d8af03544894aee26407e3bebc25addd070c
jmanywhere <josmfernandez@gmail.com>
2022-02-16 22:32:25 -0600

    mainnet presale data

--- migrations/2_deployments.js
+++ migrations/2_deployments.js
@@ -13,10 +13,10 @@ module.exports = async function ( deployer ) {
    // const Staking  = await StakeTest.deployed();
    // await deployer.deploy(Presale, NFT.address, Staking.address, Token.address);
    // TESNET DEPLOYMENT
-   await deployer.deploy(TestNFT, "Test God", "CG");
-   const NFT = await TestNFT.deployed()
+   // await deployer.deploy(TestNFT, "Test God", "CG");
+   // const NFT = await TestNFT.deployed()
    // USED TESTNET CRUSH INSTEAD OF BUSD to test
-   await deployer.deploy(Presale, NFT.address, "0x8139cA222D38296daB88d65960Ca400dcd95b246", "0xa3ca5df2938126bae7c0df74d3132b5f72bda0b6");
+   // await deployer.deploy(Presale, NFT.address, "0x8139cA222D38296daB88d65960Ca400dcd95b246", "0xa3ca5df2938126bae7c0df74d3132b5f72bda0b6");
    // MAINNET DEPLOYMENT
-   // await deployer.deploy(Presale, "CRUSH_GOD_ADDRESS", "STAKINGV2_ADDRESS", "BUSDTOKEN_ADDRESS");
+   await deployer.deploy(Presale, "0x712CC8EB5af53bfa063cc059839B6DB200Cc2536", "0x9D1Bc6843130fCAc8A609Bd9cb02Fb8A1E95630e", "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56");
 }
