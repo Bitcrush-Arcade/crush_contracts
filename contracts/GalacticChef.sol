1bf3ef0dedd1fd81e71d1c1b5b3bcd6541b773bb
jmanywhere <josmfernandez@gmail.com>
2022-03-02 20:10:36 -0600

    added Interfaces

--- contracts/GalacticChef.sol
+++ contracts/GalacticChef.sol
@@ -6,10 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./NiceToken.sol";
-
-interface IFeeDistributor{
-  function receiveFees(uint _pid, uint _amount) external;
-}
+import "./IFeeDistributor.sol";
 
 contract GalacticChef is Ownable, ReentrancyGuard {
   using SafeERC20 for NiceToken;
