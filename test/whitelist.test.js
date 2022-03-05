7897eb89a20eb4e976058dc38f326e2679c45557
jmanywhere <josmfernandez@gmail.com>
2022-03-04 20:22:17 -0600

    updated some tests

--- test/whitelist.test.js
+++ test/whitelist.test.js
@@ -50,22 +50,63 @@ contract("NFTWhitelist",([minter, user1, user2,user3, user4, receiver]) => {
   it("Should not allow reserve before wlStarts", async()=>{
 
     // reserveSpot() should fail before wl starts
-    await expectRevert(this.wl.reserveSpot({from: user1}), "Whitelist Over");
+    await expectRevert(this.wl.reserveSpot({from: user1, value: web3.utils.toWei("0.1","ether")}), "Whitelist Over");
 
     // User should be able to reserve after wl starts
     await this.wl.startWhitelist();
-    await this.wl.reserveSpot({from: user1});
-
+    await this.wl.reserveSpot({from: user1, value: web3.utils.toWei("0.1","ether")});
+    
     assert.ok(this.wl.whitelist[user1].call(), "User not added to whitelist");
     assert.equal(this.wl.allWhitelisters[0].call(), user1, "Not in list");
     assert.equal(this.wl.whitelisters, 1, "Incorrect total whitelisters");
 
   })
   
-  it("Should take only required amount from User", async()=>{})
-  it("Should fail if user sends funds just like that", async()=>{})
-  it("Should not allow reserves after whitelist ends", async()=>{})
-  it("Should lock funds for users that managed to buy", async()=>{})
+  it("Should take only required amount from User", async()=>{
+    await this.wl.startWhitelist()
+    const userInit = new BN(await web3.eth.getBalance(user1))
+    await this.wl.reserveSpot({from: user1, value: web3.utils.toWei("0.1","ether")});
+    const userFinal = new BN(await web3.eth.getBalance(user1))
+    
+    assert.equal(
+      web3.utils.fromWei(userInit.sub(userFinal)),
+      "0.1",
+      "Difference doesnt match"
+    )
+  })
+  it("Should fail if user sends funds just like that", async()=>{
+    await expectRevert(
+      web3.eth.sendTransaction({from: user1, to: this.wl.address, value: web3.utils.toWei("0.1","ether")}),
+      "Keep your money"
+    )
+  })
+  it("Should not allow reserves after whitelist ends", async()=>{
+    await this.wl.startWhitelist()
+    await this.wl.whitelistIsOver()
+    await expectRevert(
+      this.wl.reserveSpot({from: user1, value: web3.utils.toWei("0.1","ether")}),
+      "Whitelist Over"
+      )
+    })
+  it("Should lock funds for users that managed to buy", async()=>{
+    await this.wl.startWhitelist()
+    await this.wl.reserveSpot({from: user1, value: web3.utils.toWei("0.1","ether")});
+    await this.wl.reserveSpot({from: user2, value: web3.utils.toWei("0.1","ether")});
+    await this.wl.reserveSpot({from: user3, value: web3.utils.toWei("0.1","ether")});
+    await this.wl.whitelistIsOver()
+    await this.wl.updateBuyers([user1,user3],[10,20])
+    await this.wl.allBuyersAdded();
+    await expectRevert(
+      spotRefund({from: user1}),
+      "You bought an Emperor"
+      )
+    await spotRefund({from: user2})
+    await expectRevert(
+      spotRefund({from: user3}),
+      "You bought an Emperor"
+      )
+    
+  })
   it("Should allow refunds of users that did not buy", async()=>{})
   it("Should claim the funds from users that bought an NFT", async()=>{})
 })
