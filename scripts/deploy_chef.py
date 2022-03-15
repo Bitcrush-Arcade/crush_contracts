dc4c9101c1ce65d7a4506cdd8aab641436452b95
Jose Miguel Fernandez <josmfernandez@gmail.com>
2022-03-15 15:26:56 -0600

    deploy chef

--- scripts/deploy_chef.py
+++ scripts/deploy_chef.py
@@ -1,13 +1,35 @@
-from brownie import GalacticChef, FeeDistributor, accounts
+from brownie import GalacticChef, FeeDistributor, accounts, NICEToken, TokenLock
+from scripts.helpful_scripts import isDevNetwork
 
 
-def deploy_fee():
+def deploy_lock(owner):
+    return TokenLock.deploy({"from": owner})
+
+
+def deploy_fee(owner, locker):
+    # Receive Chef Address
+    # Create Fees for PoolId
     pass
 
 
-def deploy_chef():
-    pass
+def deploy_chef(owner, token_address):
+    if isDevNetwork():
+        feeAddress = accounts[1]
+    else:
+        feeAddress = accounts.load("fee_address")  # get actual address where fees go to
+    # Create Chef
+    return GalacticChef.deploy(token_address, 20, 10, 1, {"from": owner})
 
 
 def main():
+    if isDevNetwork():
+        owner = accounts[0]
+    else:
+        owner = accounts.load("main_owner")
+    nice = NICEToken[-1]
+    chef = deploy_chef(owner, nice.address)
+    # MAKE CHEF BE A MINTER
+    # Create new pool
+    locker = deploy_lock(owner)
+
     pass
