037d00b79bfdb0e6d6074f16b79dae2209fd610e
Yawgmoth19 <urzasguilt4@gmail.com>
2022-04-26 13:49:56 -0600

    Logic finished

--- contracts/InvaderAirDrop.sol
+++ contracts/InvaderAirDrop.sol
@@ -9,66 +9,68 @@ import "../MadInvaderNFT.sol";
 contract InvaderAirDrop is Ownable{
   MadInvaderNFT public immutable wrappedContract;
 
-  address[] candidateArray;
-
+  /// true = isCandidate
   mapping (address => boolean) public candidateStatus;
 
   event CandidateAdded(address);
   event CandidateArrayAdded(address[]);
   event CandidateRemoved(address);
   event CandidateArrayRemoved(address[]);
+  event InvaderDroppedTo(address);
 
   constructor(address _NFTContractAddress){
     wrappedContract = MadInvaderNFT(_NFTContractAddress);
   }
   
-  /// @notice Adds candidate to receive an invader NFT to the candidates map
+  /// @notice Adds candidate to receive an invader NFT to map
   /// @param candidateAddress is the address of said candidate
-  function addCandidate(address _candidate) external onlyOwner{
-    require (_candidateAddress != address(0) || candidates[_candidateAddress] != true || wrappedContract.balanceOf(msg.sender) < 5, "Invalid candidate");
+  function addCandidate(address _candidate) external onlyOwner {
+    require (_candidate != address(0) || candidateStatus[_candidate] != false || wrappedContract.balanceOf(_candidate) < 5, "Invalid candidate");
     candidateStatus[_candidate] = true;
     emit CandidateAdded(_candidate);
   }
 
   /// @notice Adds candidate to receive an invader NFT to the candidates map 
   /// @param candidateArray is the array of receiver candidates 
-  function addCandidateArray (address[] _candidates) external onlyOwner{
-    require(_candidates.length > 0, "No array");
+  function addCandidateArray (address[] _candidates) external onlyOwner {
+    require (_candidates.length > 0, "No array");
     for (uint i = 0; i < _candidates.length; i++){
-      if (_candidates[i] == address(0) || wrappedContract.balanceOf(msg.sender) == 5) continue;
+      if (_candidates[i] == address(0) || candidateStatus[_candidates[i]] == false || wrappedContract.balanceOf(_candidates[i]) == 5) continue;
       candidateStatus[_candidates[i]] = true;
+      emit CandidateArrayAdded(_candidates[i]);
     }
-    emit CandidateArrayAdded(_candidates);
   }
 
-  /// @notice Removes candidate from map
-  /// @param candidateAddress is the address of said candidate to remove
-  function removeCandidate(address _candidate) internal{
+  /// @notice Removes candidate from map. It doesn't delete it from map, but sets it to false so they can't become a candidate
+  /// @param candidateAddress is the address of said candidate to remove. 
+  function removeCandidate(address _candidate) external onlyOwner {
     require (_candidate != address(0), "Invalid candidate");
-    delete(candidateStatus[_candidate]);
+    candidateStatus[_candidate] = false;
     emit CandidateRemoved(_candidate);
   }
 
   /// @notice Removes candidate array from map
   /// @param candidateAddress is the address array of candidates to remove
-  function removeCandidateArray(address[] _candidates) internal{
-    require(_candidates.length > 0, "No array");
-    for(uint i = 0; i < _candidates; i++){
-      if(_candidates[i] == address(0)) continue;
-      delete(candidates[_candidates[i]]);
+  function removeCandidateArray(address[] _candidates) external onlyOwner {
+    require (_candidates.length > 0, "No array");
+    for (uint i = 0; i < _candidates; i++){
+      if (_candidates[i] == address(0)) continue;
+      candidateStatus[_candidates[i]] = false;
+      emit CandidateArrayRemoved(_candidates[i]);
     }
-    emit CandidateArrayRemoved(_candidates);
   }
 
-  /// @notice distributes the NFT's to wallets that can receive the NFT's. It resets the 
-  /// @param candidateAddress is the address array of candidates to remove
-  function dropToCandidates() external{
-    require(candidates[msg.sender] == true || wrappedContract.balanceOf(msg.sender) < 5, "Invalid candidate");
-        wrappedContract.mint(1, true);
+  /// @notice Distributes the NFT's to wallets that can receive the NFT's. Sets previous added candidates to false and cleans candidateArray
+  function dropToCandidates() external {
+    require(candidateStatus[msg.sender] == true);
+    wrappedContract.mint(1, false);
+    candidateStatus[candidateArray[i]] == false;
+    InvaderDroppedTo(msg.sender);
   }
 
+  /// @notice selfdestruct function and assets sent to owner
   function selfdestruct() external onlyOwner{
-
+    selfdestruct(msg.sender);
   }
 
 }
